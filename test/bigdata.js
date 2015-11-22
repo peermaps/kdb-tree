ef221578cb8f7a48ce96fafdbbec114cbf1b2451
James Halliday <mail@substack.net>
2015-11-21 20:04:46 -0800

    ok it definitely works now

---
+++ test/bigdata.js
@@ -0,0 +1,34 @@
+var kdbtree = require('../')
+var test = require('tape')
+
+test('big data', function (t) {
+  var n = 20000
+  var kdb = kdbtree({ dim: 3 })
+  var data = []
+  for (var i = 0; i < n; i++) {
+    var x = Math.random() * 200 - 100
+    var y = Math.random() * 200 - 100
+    var z = Math.random() * 200 - 100
+    var loc = Math.floor(Math.random() * 1000)
+    data.push({ point: [x,y,z], value: loc })
+    kdb.insert([x,y,z],loc)
+    var pts = kdb.query([x,y,z])
+    t.deepEqual(pts, [ { point: [x,y,z], value: loc } ])
+  }
+
+  var pts = kdb.query([[15,50],[-60,10],[50,100]])
+  var expected = data.filter(function (p) {
+    var pt = p.point
+    return pt[0] >= 15 && pt[0] <= 50
+      && pt[1] >= -60 && pt[1] <= 10
+      && pt[2] >= 50 && pt[2] <= 100
+  })
+  t.deepEqual(pts.sort(ptcmp), expected.sort(ptcmp))
+  t.end()
+})
+
+function ptcmp (a, b) {
+  var sa = JSON.stringify(a)
+  var sb = JSON.stringify(b)
+  return sa < sb ? -1 : 1
+}
